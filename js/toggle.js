a80cc1ce04e8c8102f2119ea4807031f57fa9544
jerico-qcu <jerico.olazo.fojas@gmail.com>
2022-12-06 14:58:37 +0800

    implement design for admin initial

--- js/toggle.js
+++ js/toggle.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("#hamburger").click(function(){
-        $(".menu-container, .container-cus, .icon-hamburger").toggleClass("magic");
+        $(".menu-container, .info, .container-cus, .icon-hamburger").toggleClass("magic");
     });
     $(".dropdown-toggle").click(function(){
         $(".dropdown-menu-cus").toggleClass("show");
@@ -13,3 +13,13 @@ $(document).ready(function(){
 function myMoveFunction() {
     $(".dropdown-menu-cus").removeClass('show');
   }
+
+$('.menu li').each(function(i){
+    $(this).hover(function(){
+        $('.menu-container, .info').addClass('show');
+    },
+    function(){
+        $('.menu-container, .info').removeClass('show');
+    }
+    );
+});
