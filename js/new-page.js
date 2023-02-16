bbd151b9b2d9ee6919bfacf51d4e7c582cbdafb8
Ericka Navarro <ericka.danielle.navarro@gmail.com>
2023-02-17 05:23:13 +0800

    update gmoral and counsel
    link redirect

---
+++ js/new-page.js
@@ -0,0 +1,20 @@
+// redirect from gmoral to schedule pickup (Good Moral)
+function gmoralRedirect() {
+    window.location.assign('admin-gpup.html');
+ }
+
+// redirect from schedule pickup to Good Moral (Good Moral)
+function schedgpupRedirect() {
+    window.location.assign('admin-gmoral.html');
+ }
+
+
+ // redirect from counsel to schedule counseling (Counselling)
+function counselRedirect() {
+    window.location.assign('admin-schedCounsel.html');
+ }
+
+  // redirect from schedule counseling to counsel (Counselling)
+function schedCounselRedirect() {
+    window.location.assign('admin-counsel.html');
+ }
